f2fc59253d9676ce95877f51e7dafc274ea1ea8e
Kevin Roney <ktroney.pdx@gmail.com>
2022-06-08 16:16:55 -0700

    wrote test function testing getAll functionality

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,11 +3,17 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const Band = require('../lib/models/Band');
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-
+  it('GET /bands responds with an array of bands', async () => {
+    const res = await request(app).get('/bands');
+    const bands = await Band.getAll();
+    expect(res.body).toEqual(bands);
+  });
   afterAll(() => {
     pool.end();
   });
